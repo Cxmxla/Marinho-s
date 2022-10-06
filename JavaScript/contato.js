41dedc4fba3ef48cb7d890c6d91681634a7fde68
Cxmxla <114073795+Cxmxla@users.noreply.github.com>
2022-10-06 11:33:09 -0300

    Add files via upload

---
+++ JavaScript/contato.js
@@ -0,0 +1,9 @@
+const send = document.getElementById('send')
+
+send.addEventListener('click',(event) => {
+    event.preventDefault()
+
+    const email = document.getElementById('email')
+
+    if(email.value)
+})
