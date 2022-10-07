eb2a06122109ea0eb6948bdb8ceecea7279a7f7d
Cxmxla <114073795+Cxmxla@users.noreply.github.com>
2022-10-06 22:34:50 -0300

    Delete JavaScript directory

--- JavaScript/contato.js
+++
@@ -1,9 +0,0 @@
-const send = document.getElementById('send')
-
-send.addEventListener('click',(event) => {
-    event.preventDefault()
-
-    const email = document.getElementById('email')
-
-    if(email.value)
-})
